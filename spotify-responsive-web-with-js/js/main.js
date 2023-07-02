3dd3898ac40c8fd0ed0e9f114548d7564d72f02f
Uğurtan <ugurtan.52@gmail.com>
2023-07-02 20:05:46 +0300

    Js

--- spotify-responsive-web-with-js/js/main.js
+++
@@ -1,9 +0,0 @@
-const btnHamburgerEl = document.querySelector(".btn-hamburger");
-const wrapperHamburger = document.querySelector(".wrapper-hamburger-nav");
-
-btnHamburgerEl.addEventListener("click", handleNavHamburgerToggle);
-
-
-function handleNavHamburgerToggle(){
-    wrapperHamburger.classList.toggle("on");
-}
